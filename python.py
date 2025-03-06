920f1ea88ac8e4abefd87621e33e6b458664b8c4
unknown <gilmar.cruz@grupofametro.com.br>
2025-03-06 16:46:56 -0400

    enviando arquivos

---
+++ python.py
@@ -0,0 +1,11 @@
+print("hello world") 
+# são os tipos de dados em python
+
+# textos são str
+# numericos são int, float, complex
+# sequencia são list, tuple, range
+# mapa são dict
+# coleção são set, fronzenset
+# booleano são bool
+# binario são bytes, bytearray, memoryview
+
