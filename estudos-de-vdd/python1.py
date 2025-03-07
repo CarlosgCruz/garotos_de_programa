e3e2e0b6b41c0caf8f8dd7fa892beac135b164e1
carlos <gilmarcarlos711@gmail.com>
2025-03-07 12:40:11 -0400

    add repo

---
+++ estudos-de-vdd/python1.py
@@ -0,0 +1,154 @@
+# operadores de comparação
+
+# operadores de igualdade
+
+saldo = 450
+saque = 200
+
+print(saldo == saque)
+
+# opr de diferenca
+
+ 
+saldo = 450
+saque = 200
+
+print(saldo == saque)
+
+# maior que/ maior ou igual
+
+ 
+saldo = 450
+saque = 200
+print(saldo > saque)
+
+print(saldo >= saque)
+
+# menor que/ menor ou igual
+
+ 
+saldo = 450
+saque = 200
+print(saldo < saque)
+
+print(saldo <= saque)
+
+# operadores de atribuição
+
+# atribuição simples
+
+saldo = 500 
+
+print(saldo)
+
+# atribuição com adição 
+
+saldo = 500
+saldo += 200 
+
+print(saldo)
+
+# atribuição com subtração
+
+saldo = 500
+saldo -= 200 
+
+print(saldo)
+
+# atribuição com multiplicação
+
+saldo = 500
+saldo *= 2
+
+print(saldo)
+
+# atribuição com divisão
+
+saldo = 500
+saldo /= 5
+
+print(saldo)
+
+saldo = 500
+saldo //= 5 
+
+print(saldo)
+
+# atribuição com módulo]
+
+saldo = 500
+saldo %= 480 
+
+print(saldo)
+
+# atribuição com exponenciação
+
+saldo = 80
+saldo **= 2 
+
+print(saldo)
+
+# OPERADORES LÓGICOS
+
+saldo = 1000
+saque = 200 
+limite = 100
+
+saldo >= saque
+
+saque <= limite
+
+# operador ou (or)
+
+saldo = 1000
+saque = 200 
+limite = 100
+
+saldo >= saque or saque <= limite
+
+# operador de negação
+
+contatos_emergencia =[]
+
+not 1000 > 1500
+
+not contatos_emergencia
+
+not "saque 1500"
+
+not ""
+
+# parenteses
+
+saldo = 1000
+saque = 200 
+limite = 100
+conta_especial = True
+
+saldo >= saque and saque <= limite or conta_especial and saldo >= saque
+
+(saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque)
+
+# OPERADORES DE INDENTIDADE
+
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200, 200
+
+curso is nome_curso
+
+curso is not nome_curso
+
+saldo is limite
+
+# OPERADODORES DE ASSOCIANÇÃO
+
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+"Python" in curso
+
+"maça" not in frutas
+
+200 in saques 
