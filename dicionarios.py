b2d27e009511d9e7cea183ac58067fdadbf786f5
unknown <gilmar.cruz@grupofametro.com.br>
2025-03-26 11:28:05 -0400

    add content

--- dicionarios.py
+++ dicionarios.py
@@ -18,5 +18,47 @@ pessoa["telefone"] = "3333-1235"
 
 print(pessoa)
 
+# acesso aos dados
+# os dados são acessados e modificados atraves da chave
+
+dados = {"nome": "gilmar", "idade": 21, "telefone": "3333-1234"}
+
+print(dados["nome"])  # "gilmar"
+print(dados["idade"])  # 21
+print(dados["telefone"])  # "3333-1234"
+
+dados["nome"] = "Maria"
+dados["idade"] = 18
+dados["telefone"] = "9988-1781"
+
+print(dados)  # {"nome": "Maria", "idade": 18, "telefone": "9988-1781"}
+
+# dicionarios aninhados
+"""
+dicionarios pode armazenar qualquer tipo de objeto pyhton como valor, desde que a 
+chave para esse valor seja um objeto imutavel como (strings e numeros)
+"""
+
+contatos = {
+    "joao@gmail.com": {"nome": "joao", "telefone": "3333-2221"},
+    "jorge@gmail.com": {"nome": "jorge", "telefone": "3443-2121"},
+    "carlos@gmail.com": {"nome": "carlos", "telefone": "3344-9871"},
+    "giovana@gmail.com": {"nome": "giovana", "telefone": "3333-7766"},
+}
+
+telefone = contatos["giovana@gmail.com"]["telefone"]  # "3443-2121"
+print(telefone)
+
+# iterar dicionarios
+# a forma mais comum para percorrer os dados de um dicionario é utilizando o comando for 
+
+for chave in contatos:
+    print(chave, contatos[chave])
+
+print("=" * 100)
+
+for chave, valor in contatos.items():
+    print(chave, valor)
+
 
 # metodos da classe dict
